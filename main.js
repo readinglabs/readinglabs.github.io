b27b90d221a2f69d81ab2209517fd5825b033a27
readinglabs <160203760+readinglabs@users.noreply.github.com>
2024-02-15 20:31:41 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 alert('Notice that this is only for school use. You must have a sandi unified email to login.')
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCD5nn8aToFhG4EcnaH_XxFXIfD87yuLyo",
-    authDomain: "amplifylogin2.firebaseapp.com",
-    projectId: "amplifylogin2",
-    storageBucket: "amplifylogin2.appspot.com",
-    messagingSenderId: "12181983273",
-    appId: "1:12181983273:web:8e3a08c63a4a14b4708c38",
-    measurementId: "G-1ZSN19WV77"
-  };
+  apiKey: "AIzaSyDpkt3ZEJAcdihIC1aFwB2tdWhLHI1UTes",
+  authDomain: "logintoamplify.firebaseapp.com",
+  projectId: "logintoamplify",
+  storageBucket: "logintoamplify.appspot.com",
+  messagingSenderId: "1081201855286",
+  appId: "1:1081201855286:web:99d752e5e8caab5f79c197"
+};
 
   firebase.initializeApp(firebaseConfig);
 
